62548e1a7c4c42168f36cb70bd2de32098513200
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-09-19 09:19:53 +0700

    javascriptDOM (Document Object Model)

--- IntroducingJavascript/index.js
+++ IntroducingJavascript/index.js
@@ -1,1 +1,10 @@
 console.log('Hello Javascript in another file!');
+
+// Akan menampillkan document HTML
+console.log(document); 
+
+// Akan mememberitahukan type document adalah Object
+console.log(typeof document);
+
+// Ini adalah bentuk dasar dari Javascript DOM
+document.getElementById('app').innerHTML = 'Javascript Rules';
