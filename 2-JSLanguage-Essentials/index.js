c99b480fe9fb0f45adcacd13f03988c80bc5b2d6
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-09-19 09:47:58 +0700

    Memahami fungsi variable dan penugasannya

---
+++ 2-JSLanguage-Essentials/index.js
@@ -0,0 +1,13 @@
+// Syntax , Statement and Data Types
+
+var a, b;
+a = 'Hello';
+b = a + "World";
+console.log(b);
+// Maka return b = Hello World
+// Tanda kutip untuk string boleh satu atau 2
+// kalo buat number tidak usah pake tanda kutip
+
+// Ciri variables dan tugasnya
+// Nama Variabel tidak boleh 'var', pisah
+// ini boleh $_a, Faza_Fahamsyah, FazaFahamsyah
