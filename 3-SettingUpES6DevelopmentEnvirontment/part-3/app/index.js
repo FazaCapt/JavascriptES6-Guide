43fbf1a189a87eae0a84212a6b773efa5887973b
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-09-30 10:58:02 +0700

    Map and filter method

--- 3-SettingUpES6DevelopmentEnvirontment/part-3/app/index.js
+++ 3-SettingUpES6DevelopmentEnvirontment/part-3/app/index.js
@@ -32,4 +32,45 @@ let cheers = () => {
 }
 
 // console.log(cheers);
-cheers();
+// cheers();
+
+
+// ==================== Map and filter method =========
+// ==== Map
+let values = [20, 30, 40];
+
+// ==== Cara 1:
+// let double = (n) => {
+//     return n * 2;
+// }
+
+// let doubled = values.map(double);
+// console.log(doubled);
+
+// ==== Cara 2:
+
+// let doubled = values.map((n) => {
+//     return n * 2;
+// });
+
+// console.log(doubled);
+
+// ==== Cara 3:
+
+// let doubled = values.map((n) => n * 3);
+
+// console.log(doubled);
+
+// ===== Filter
+
+let points = [13, 25, 32, 56, 70, 75, 80, 89, 90];
+
+// === Cara 1
+// let highScores = points.filter((n) => {
+//     return n < 50;
+// });
+
+// === Cara 2
+let highScores = points.filter((n) => n > 50);
+
+console.log(highScores);
