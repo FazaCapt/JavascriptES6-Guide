4bd7b3fa569f6219b93a7d2edfd4330ce7253945
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-10-02 07:19:06 +0700

    Function Factories

--- 3-SettingUpES6DevelopmentEnvirontment/part-6(Clouser)/app/index.js
+++ 3-SettingUpES6DevelopmentEnvirontment/part-6(Clouser)/app/index.js
@@ -20,4 +20,41 @@ let call = () => {
 // call();
 
 let unveil = call();
-unveil();
+// unveil();
+
+// ==== Function Factories
+
+const addSuffix = (x) => {
+    const concat = (y) => {
+        return y + x;
+    }
+    return concat;
+}
+
+// let add_ness = addSuffix('ness');
+// console.log(add_ness);
+
+// let h = add_ness('Happi');
+// console.log(h);
+
+// let add_full = addSuffix('Full');
+// let h = add_full('Fruit');
+// console.log(h);
+
+// ==== Contoh 2
+
+// const product = (x) => {
+//     return y => {
+//         return y * x;
+//     }
+// }
+
+// ==== Ubah bentuk menjadi:
+
+const product = x => y => y * x;
+
+let mult5 = product(5);
+console.log(mult5(3));
+
+let double = product(2);
+console.log(double(9));
